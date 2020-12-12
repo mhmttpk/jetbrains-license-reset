ce1a7bee50fb110970ecbe59345d6bc921715c63
mhmttpk <59623440+mhmttpk@users.noreply.github.com>
2020-12-12 10:19:02 +0300

    Add files via upload

---
+++ resetAll.sh
@@ -0,0 +1,3 @@
+./webstorm.sh
+./phpstorm.sh
+./datagrip.sh
---
+++ webstorm.sh
@@ -0,0 +1,7 @@
+#WEB STORM 
+cd ~/.config/JetBrains/WebStorm2020.3
+rm - rf ${HOME}/.config/JetBrains/WebStorm2020.3/eval/WebStorm203.evaluation.key 
+cd ~/.java/.userPrefs/jetbrains 
+rm -rf webstorm 
+rm -rf ~/.config/JetBrains/WebStorm
+
