bad86f69836fda244ad0eb7c4dc67072e2a1a919
mhmttpk <59623440+mhmttpk@users.noreply.github.com>
2021-01-07 09:35:39 +0300

    Update phpstorm.sh

--- phpstorm.sh
+++ phpstorm.sh
@@ -1,6 +1,6 @@
 #PHP STORM 
 cd ~/.config/JetBrains/PhpStorm2020.3
-rm - rf ${HOME}/.config/JetBrains/PhpStorm2020.3/eval/PhpStorm203.evaluation.key
+rm -rf ${HOME}/.config/JetBrains/PhpStorm2020.3/eval/PhpStorm203.evaluation.key
 cd ~/.java/.userPrefs/jetbrains 
 rm -rf phpstorm 
 rm -rf ~/.config/JetBrains/PhpStorm
