132cd0baa00e8c85562dcbee85cba0d060acf8ad
mhmttpk <59623440+mhmttpk@users.noreply.github.com>
2021-01-07 09:35:23 +0300

    Update datagrip.sh

--- datagrip.sh
+++ datagrip.sh
@@ -1,6 +1,6 @@
 #DATAGRIP
 cd ~/.config/JetBrains/DataGrip2020.3
-rm - rf ${HOME}/.config/JetBrains/DataGrip2020.3/eval/DataGrip203.evaluation.key
+rm -rf ${HOME}/.config/JetBrains/DataGrip2020.3/eval/DataGrip203.evaluation.key
 cd ~/.java/.userPrefs/jetbrains 
 rm -rf datagrip 
 rm -rf ~/.config/JetBrains/DataGrip
